94d26ea241332210b3fe7cbd23f004c76a051e73
Xavier <xaok7569@colorado.edu>
2023-11-13 19:14:46 -0700

    added logo

---
+++ docs/_build/html/_sources/maestro.md.txt
@@ -0,0 +1,3 @@
+# Documentation for Maestro 
+This was a massive pain for us so we've tried to document it.
+
--- docs/conf.py
+++ docs/conf.py
@@ -27,3 +27,4 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = "sphinx_rtd_theme"
 # html_static_path = ['_static']
 html_theme_path = ["_themes",]
+html_logo = "_static/Colorado_Robosub_Text.png"
