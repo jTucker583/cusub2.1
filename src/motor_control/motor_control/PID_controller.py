378e90db36b2201b944c9cf5c0d2be03c1913656
William Hardee <williamhardee6@gmail.com>
2024-04-17 19:32:46 -0600

    initial PID loop, only uses P value

--- src/motor_control/motor_control/PID_controller.py
+++ src/motor_control/motor_control/PID_controller.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 
+import math
+
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Pose
 
@@ -12,16 +14,49 @@ class pid_controller(Node):
         timer_period = 0.5  # seconds
         self.goalpose = self.create_subscription(Pose, 'goal_pose', self.controller_callback, 10)
         self.currentpose = self.create_subscription(Pose, 'pose', self.current_pose_callback, 10)
+        self.goal = Pose()
+        self.currentPosition = Pose()
+        self.Kp = 1
         self.i = 0
 
-    def controller_callback(self):
+    def controller_callback(self, msgPose):
+        self.goal = msgPose
+
+    def current_pose_callback(self, msg):
+        self.currentPosition = msg
+
+    def at_goal(self):
+        return (self.goal.position.x == self.currentPosition.position.x and 
+                self.goal.position.y == self.currentPosition.position.y and
+                self.goal.position.z == self.currentPosition.position.z)
+    
+    def pidLoop(self):
         msg = Twist()
-        msg.linear.x = 69.420
-        msg.linear.y = 420.69
-        self.publisher_.publish(msg)
-        self.i += 1
-    def current_pose_callback(self):
-        pass
+        while(not self.at_goal):
+            diff = self.getDistance
+
+            if(self.currentPosition.position.x < self.goal.position.x):
+                msg.linear.x = self.Kp * diff
+            else:
+                msg.linear.x = -self.Kp * diff
+            if(self.currentPosition.position.y < self.goal.position.y):
+                msg.linear.y = self.Kp * diff
+            else:
+                msg.linear.y = -self.Kp * diff
+            if(self.currentPosition.position.z < self.goal.position.z):
+                msg.linear.z = self.Kp * diff 
+            else:
+                msg.linear.z = -self.Kp * diff
+            
+
+            self.publisher_.publish(msg)
+
+    def getDistance(self):
+        return math.sqrt((self.goal.position.x - self.currentPosition.position.x)**2 +
+                         (self.goal.position.y - self.currentPosition.position.y)**2 +
+                         (self.goal.position.y - self.currentPosition.position.y)**2)
+    
+                    
 
 
 def main(args=None):
